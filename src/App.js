1f7b4ba1dc0a2eea04231d88d271abcb553ea867
Ravi Kumar <rkumrpb@amazon.com>
2022-02-28 21:58:14 -0800

    App js commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './Hero';
+import Shaktiman from './Shaktiman';
+import MySample from './Sample';
+import MyGame from './MyGame';
+import React from 'react';
+import ScoreCard from './ScoreCard';
+import ErrorBoundry from './Error';
+import FragmentDemo from './FragmentDemo';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app-wrapper">
+        <ErrorBoundry>
+          <Hero name="Shaktiman"  country = "India"/>
+        </ErrorBoundry>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <ErrorBoundry>
+            <Hero name="CC"  country = "India"/>
+        </ErrorBoundry>
+
+        <ErrorBoundry>
+              <Hero name="Nagraj"  country = "India"/>
+        </ErrorBoundry>
+
+        <ErrorBoundry>
+              <Hero name="Shakal"  country = "India"/>
+        </ErrorBoundry>
+
+        <FragmentDemo/>
+      </div>
+     
+    );
+  }
+  
 }
 
 export default App;
